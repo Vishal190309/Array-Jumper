f3d6d0465fdcc6e771dcfbbe1d78873eb399bf76
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-07-20 01:56:19 +0530

    Create Multiple Levels

---
+++ Array-Jumper/header/Level/LevelController.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "LevelModel.h"
+
+namespace Level
+{
+	class LevelView;
+	class LevelModel;
+
+	class LevelController
+	{
+	private:
+		LevelView* level_view;
+		LevelModel* level_model;
+
+	public:
+		LevelController();
+		~LevelController();
+
+		void initialize();
+		void update();
+		void render();
+
+		BlockType getCurrentBoxValue(int currentPosition);
+		BoxDimensions getBoxDimensions();
+
+		bool isLastLevel();
+		void loadNextLevel();
+		int getCurrentLevelNumber();
+		void reset();
+	};
+}
---
+++ Array-Jumper/header/Level/LevelService.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "../../header//Level/BlockType.h"
+namespace Level
+{
+	class LevelController;
+	struct BoxDimensions;
+	
+	class LevelService
+	{
+	private:
+
+		LevelController* level_controller;
+		void destroy();
+
+	public:
+		LevelService();
+		~LevelService();
+
+		void initialize();
+		void update();
+		void render();
+
+		BoxDimensions getBoxDimensions();
+		BlockType getCurrentBoxValue(int currentPosition);
+
+		bool isLastLevel();
+		void loadNextLevel();
+		int getCurrentLevelNumber();
+		void reset();
+		
+
+	};
+}
